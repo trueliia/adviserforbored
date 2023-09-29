2ea57f50bfffb1f66d0b398596046d75d5f36694
Julia Naumova <140359113+trueliia@users.noreply.github.com>
2023-09-29 06:24:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 }, [])
 
 const getActivity = async () =>{
-  const response = await fetch("http://www.boredapi.com/api/activity/");
+  const response = await fetch("https://www.boredapi.com/api/activity/");
   const data = await response.json();
   setActivity(data.activity)
 }
